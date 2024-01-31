f15e7d63b7aee2baf9a3c9416ee118c22d25264e
Baikyuuu <quentinguicherd@hotmail.fr>
2024-01-31 16:10:22 +0100

    v4

--- Quentin/OU.ps1
+++ Quentin/OU.ps1
@@ -1,27 +1,17 @@
-Write-Host "# Création d'uue OU dans le domaine $((Get-ADDomain).DNSRoot)" -ForegroundColor Cyan
-Write-Host "# Veuillez renseigner les différents champs ci-dessous" -ForegroundColor Cyan
-$OU1 = Read-Host -Prompt "OU1" 
-$OU2 = Read-Host -Prompt "OU2 after OU1"
-$OU3 = Read-Host -Prompt "OU3 after OU1"
-$OU4 = Read-Host -Prompt "OU4 after OU1"
-#The command below uses the New-ADOrganizationUnit cmdlet to create a root OU called $OU1
-New-ADOrganizationalUnit -Path 'DC=tssr,DC=acab' -Name '$OU1' -Verbose
+ $OUExist = [adsi]::Exists("LDAP://OU=TESTING,OU=Computers,OU=DomainName,dc=Domain,dc=RootDomain")
+if ($OUExist)
+{
+    "It already exists"
+} Else {
+    "Creating the OU"
+    New-ADOrganizationalUnit -Name 'TESTING' -Path "OU=Computers,OU=DomainName,dc=Domain,dc=RootDomain"
+}
 
-#The commands below use the New-ADOrganizationUnit cmdlet to create three new sub 
-#OUs called $OU2, OU3, and $OU4
 
-New-ADOrganizationalUnit -Path 'OU=$OU1,DC=tssr,DC=acab' -Name '$OU2' -Verbose
-New-ADOrganizationalUnit -Path 'OU=$OU1,DC=tssr,DC=acab' -Name '$OU3' -Verbose
-New-ADOrganizationalUnit -Path 'OU=$OU1,DC=tssr,DC=acab' -Name '$OU4' -Verbose
-
-#The commands below use the New-ADOrganizationUnit cmdlet to create two new sub OUs for 
-#Users and Computers under the Nest OUs that you created earlier
-
-New-ADOrganizationalUnit -Path 'OU=$OU2,OU=$OU1,DC=tssr,DC=acab' -Name 'Users' -Verbose
-New-ADOrganizationalUnit -Path 'OU=$OU2,OU=$OU1,DC=tssr,DC=acab' -Name 'Computers' -Verbose
-
-New-ADOrganizationalUnit -Path 'OU=OU3,OU=$OU1,DC=tssr,DC=acab' -Name 'Users' -Verbose
-New-ADOrganizationalUnit -Path 'OU=OU3,OU=$OU1,DC=tssr,DC=acab' -Name 'Computers' -Verbose
-
-New-ADOrganizationalUnit -Path 'OU=$OU4,OU=$OU1,DC=tssr,DC=acab' -Name 'Users' -Verbose
-New-ADOrganizationalUnit -Path 'OU=$OU4,OU=$OU1,DC=tssr,DC=acab' -Name 'Computers' -Verbose
+ $OUExist = [adsi]::Exists("LDAP://OU=TESTING,OU=Computers,OU=DomainName,dc=Domain,dc=RootDomain")
+if ($OUExist)
+{
+    "OU already exists"
+} Else {
+    "OU does NOT exist"
+}-ADOrganizationalUnit -Path 'OU=$OU4,OU=$OU1,DC=tssr,DC=acab' -Name 'Computers' -Verbose
